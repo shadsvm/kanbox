a9cd9030572d645de181c2289449e2200f4a8b92
Samuel <91400290+sam-kmn@users.noreply.github.com>
2023-02-20 19:50:41 +0100

    🔧 Client error response page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,8 +4,8 @@ const UnauthorizedPage = () => {
   return (
     <Layout>
       <div className="flex flex-1 flex-col items-center justify-center gap-3">
-        <h1 className="text-4xl font-semibold sm:text-5xl">You are unauthorized!</h1>
-        <h3 className="text-lg text-gray-400 sm:text-xl">Owner of this board set permissions to private</h3>
+        <h1 className="text-3xl font-semibold sm:text-4xl">This board doesn't exists!</h1>
+        <h3 className="text-lg text-gray-400 sm:text-xl">Make sure you typed URL right</h3>
       </div>
     </Layout>
   )
